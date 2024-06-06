31a39698211cc3a47965febdf0393a46f9236d1c
cliwanag07 <cliwanag@csu.fullerton.edu>
2024-06-06 14:44:55 -0700

    include order fixed

--- Source/EchoesOfHubris/Private/SortActorArrByDistToActor.cpp
+++ Source/EchoesOfHubris/Private/SortActorArrByDistToActor.cpp
@@ -3,19 +3,15 @@
 
 TArray<AActor*> USortActorArrByDistToActor::SortArrayByActorDistance(const TArray<AActor*>& ActorArray, AActor* ReferenceActor)
 {
-	if (!ReferenceActor)
-	{
-		// If the reference actor is null, return the original array
-		return ActorArray;
-	}
+    if (!ReferenceActor)
+    {
+        return ActorArray; // If no reference actor is provided, return the array as is
+    }
 
-	TArray<AActor*> SortedArray = ActorArray;
+    TArray<AActor*> SortedArray = ActorArray;
+    SortedArray.Sort([ReferenceActor](const AActor& A, const AActor& B) {
+        return ReferenceActor->GetDistanceTo(&A) < ReferenceActor->GetDistanceTo(&B);
+        });
 
-	SortedArray.Sort([ReferenceActor](const AActor& A, const AActor& B) {
-		float DistanceA = FVector::Dist(ReferenceActor->GetActorLocation(), A.GetActorLocation());
-		float DistanceB = FVector::Dist(ReferenceActor->GetActorLocation(), B.GetActorLocation());
-		return DistanceA < DistanceB;
-		});
-
-	return SortedArray;
+    return SortedArray;
 }
--- Source/EchoesOfHubris/Public/SortActorArrByDistToActor.h
+++ Source/EchoesOfHubris/Public/SortActorArrByDistToActor.h
@@ -10,7 +10,9 @@
 UCLASS()
 class ECHOESOFHUBRIS_API USortActorArrByDistToActor : public UBlueprintFunctionLibrary
 {
-	GENERATED_BODY()
-		UFUNCTION(BlueprintCallable, Category = "Sort Actor Array by Distance to Actor")
-		static TArray<AActor*> SortArrayByActorDistance(const TArray<AActor*>& ActorArray, AActor* ReferenceActor);
+    GENERATED_BODY()
+
+public:
+    UFUNCTION(BlueprintCallable, Category = "Sort Actor Array by Distance to Actor")
+    static TArray<AActor*> SortArrayByActorDistance(const TArray<AActor*>& ActorArray, AActor* ReferenceActor);
 };
