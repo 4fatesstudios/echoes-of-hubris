8625c705959d9a8e1e508d2bad2a7db0d85ab0b5
Chris Liwanag <143138753+cliwanag07@users.noreply.github.com>
2025-01-10 18:51:47 -0800

    init game instance

---
+++ Source/EchoesOfHubris/Public/SinglePlayer.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "SinglePlayer.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ECHOESOFHUBRIS_API USinglePlayer : public UGameInstance
+{
+	GENERATED_BODY()
+	
+};
